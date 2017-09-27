592530301d2f538246cde333da2c80a11d4783ce
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-09-28 01:24:15 +0300

    Add sanity check as a test

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
+set -u
 
 ### globals ####################################################################
 
 this_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-# TODO: run tests
+export RESULTS_PATH="$(mktemp -d -t rfdocker-tests-XXXXXX)"
+echo -e "[rfdocker] Using a temporary RESULTS_PATH for tests: $RESULTS_PATH\n"
+
+TESTS_PATH=../tests "$this_path/../bin/rfdocker" tests
