88d4e9604129327ee319c29e8c68bdeb305876e1
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-09-28 14:06:22 +0300

    Parametrize script to take the Docker repository url

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,9 +9,20 @@ rf_version=$(cat "$this_path/../rf_version")
 
 ### main #######################################################################
 
-git tag --force --annotate \
-  --message "Release $rf_version" \
-  "$rf_version"
+if [ -z "$1" ]; then
+  echo "Usage: $0 REPOSITORY_URL"
+  echo ""
+  echo "  e.g.: $0 https://your.registry.com:5000/rfdocker"
+  exit 64
+fi
+
+echo "### Creating a git tag '$rf_version' for the release"
+git tag --force --annotate --message "Release $rf_version" "$rf_version"
+
+repository_url="$1"
+
+echo "### Tagging image 'rfdocker:$rf_version' to '$repository_url:$rf_version'"
+docker tag "rfdocker:$rf_version" "$repository_url:$rf_version"
 
 echo "### Pushing image 'rfdocker:$rf_version' to registry..."
-docker push "rfdocker:$rf_version"
+docker push "$repository_url:$rf_version"
