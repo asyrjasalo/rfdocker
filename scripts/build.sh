b4a7018dcfa819f1bc69cfb3f5fc862ffb8ab57f
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-09-28 13:33:17 +0300

    Add support for different Robot Framework versions

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,11 @@ set -e
 ### globals ####################################################################
 
 this_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+rf_version=$(cat "$this_path/../rf_version")
 
 ### main #######################################################################
 
-docker build -t rfdocker:"${1:-latest}" "$this_path/../docker"
+docker build --rm \
+  --build-arg RF_VERSION="$rf_version" \
+  -t "rfdocker:$rf_version" \
+  "$this_path/../docker"
