98fae06430f827bbb3470a7b0e28994bd96c4da0
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-10-11 19:28:06 +0300

    Parametrize Python version to build on

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 set -e
+set -u
 
 ### globals ####################################################################
 
 this_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-release_name=$(cat "$this_path/../release_name")
+python_version="$(cat "$this_path/../python_version")"
+release_name="$(cat "$this_path/../release_name")"
+robotframework_version="$(echo "$release_name" | cut -d '-' -f 1)"
 
 ### main #######################################################################
 
-docker build --rm \
-  --build-arg RF_VERSION="$(echo $release_name | cut -d '-' -f 1)" \
-  -t "rfdocker:$release_name" \
+docker build \
+  --build-arg PYTHON_VERSION="$python_version" \
+  --build-arg ROBOTFRAMEWORK_VERSION="$robotframework_version" \
+  --tag "rfdocker:$release_name" \
   "$this_path/../docker"
