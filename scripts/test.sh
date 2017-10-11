25db9459174261310002a17be6a7855db98f08ec
Anssi Syrjäsalo <anssi.syrjasalo@gmail.com>
2017-10-11 19:30:00 +0300

    Tidy up arguments

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,22 +1,17 @@
 #!/usr/bin/env bash
 
 set -e
-set -u
 
 ### globals ####################################################################
 
 this_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+release_name="$(cat "$this_path/../release_name")"
 temp_results_path="/tmp/rfdocker-tests"
-release_name=$(cat "$this_path/../release_name")
 
 ### main #######################################################################
 
 mkdir -p "$temp_results_path"
 
-docker run --rm -ti \
-  -e UID=$(id -u) \
-  -e GID=$(id -g) \
-  -v "$temp_results_path":/home/robot/results \
-  -v "$this_path/../tests":/home/robot/tests \
-    "rfdocker:$release_name" \
-    tests
+docker run --rm -ti -e UID=$UID -e GID=$GID \
+  -v "$this_path/..":/home/robot \
+  "rfdocker:$release_name" tests/
